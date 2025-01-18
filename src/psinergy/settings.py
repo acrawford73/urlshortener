0b7134677a29bcbdafa3d92faa555d7c1d2186a7
Anthony Crawford <crawfishmedia@gmail.com>
2025-01-18 16:27:53 -0400

    .

--- src/psinergy/settings.py
+++ src/psinergy/settings.py
@@ -128,7 +128,10 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
-        'CONN_MAX_AGE': 600,
+        #'CONN_MAX_AGE': 600,
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
