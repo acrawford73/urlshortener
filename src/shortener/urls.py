356eb5757460eedef0f3fbc30b291b9cfee139ee
Anthony Crawford <crawfishmedia@gmail.com>
2025-01-23 19:31:31 -0400

    .

--- src/shortener/urls.py
+++ src/shortener/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
 	path('create/', views.ShortenerCreateView.as_view(), name='shortener-create'),
 	path('update/<int:pk>/', views.ShortenerUpdateView.as_view(), name='shortener-update'),
 	path('short/<int:pk>/', views.ShortenerDetailView.as_view(), name='shortener-detail'),
-	path('mylinks/', views.ShortenerListView.as_view(), name='my-links-list'),
-	path('toplinks/', views.ShortenerTopListView.as_view(), name='top-links-list'),
+	path('mylinks/', views.ShortenerListView.as_view(), name='shortener-list'),
+	path('toplinks/', views.ShortenerTopListView.as_view(), name='top-shortener-list'),
 
 	path('shorten/', views.shorten_url, name='shorten-url'),
 	path('<str:alias>/', views.redirect_url, name='redirect-url'),
