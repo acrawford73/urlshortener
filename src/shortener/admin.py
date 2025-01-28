4faf683c5a1412a57b07e74f6ea002f315367155
Anthony Crawford <crawfishmedia@gmail.com>
2025-01-28 17:48:34 -0400

    .

--- src/shortener/admin.py
+++ src/shortener/admin.py
@@ -3,7 +3,7 @@ from .models import ShortURL
 
 
 class ShortURLAdmin(admin.ModelAdmin):
-	list_display = ['short_alias', 'created_at', 'clicks']
+	list_display = ['short_alias', 'clicks', 'owner', 'created_at']
 	search_fields = ['short_alias']
 	class Meta:
 		model = ShortURL
