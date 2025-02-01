066f2462ff8000155f4c5e74157e278e5494f414
Anthony Crawford <crawfishmedia@gmail.com>
2025-02-01 14:46:19 -0400

    .

--- src/static_files/js/copy-button.js
+++ src/static_files/js/copy-button.js
@@ -1,18 +1,20 @@
 // copy-button
 // Get references to the HTML elements
 const copyBtn = document.getElementById('copyBtn');
-const textToCopy = document.getElementById('shortAlias');
+const textToCopy1 = document.getElementById('shortTitle');
+const textToCopy2 = document.getElementById('shortAlias');
 
 // Add click event listener to the button
 copyBtn.addEventListener('click', () => {
 	// Extract the text from the element
-	const text = textToCopy.innerText;
+	const text1 = textToCopy1.innerText;
+	const text2 = textToCopy2.innerText;
 
 // Use the Clipboard API to write the text to the clipboard
-navigator.clipboard.writeText(text)
+navigator.clipboard.writeText(text1+"/n"+text2)
 	.then(() => {
 		// Optional: Provide feedback to the user
-		alert('Text copied to clipboard!');
+		// alert('Text copied to clipboard!');
 	})
 	.catch(err => {
 		console.error('Failed to copy: ', err);
