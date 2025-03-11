3f83f405e01e3c6ade2f32b6a504c1a7c1723027
Anthony Crawford <crawfishmedia@gmail.com>
2025-03-10 23:36:25 -0300

    .

--- src/shortener/owner.py
+++ src/shortener/owner.py
@@ -6,12 +6,24 @@ class OwnerListView(LoginRequiredMixin, ListView):
     """
     Sub-class the ListView to pass the request to the form.
     """
+    def get_queryset(self):
+        qs = super(OwnerListView, self).get_queryset()
+        if self.request.user.is_staff:
+            return qs
+        else:
+            return qs.filter(owner=self.request.user)
 
 
 class OwnerDetailView(LoginRequiredMixin, DetailView):
     """
     Sub-class the DetailView to pass the request to the form.
     """
+    def get_queryset(self):
+        qs = super(OwnerDetailView, self).get_queryset()
+        if self.request.user.is_staff:
+            return qs
+        else:
+            return qs.filter(owner=self.request.user)
 
 
 class OwnerCreateView(LoginRequiredMixin, CreateView):
@@ -39,7 +51,6 @@ class OwnerUpdateView(LoginRequiredMixin, UpdateView):
         #print('update get_queryset called')
         """ Limit a User to only modifying their own data. """
         qs = super(OwnerUpdateView, self).get_queryset()
-        # return qs.filter(owner=self.request.user)
         if self.request.user.is_staff:
             return qs
         else:
@@ -55,7 +66,6 @@ class OwnerDeleteView(LoginRequiredMixin, DeleteView):
     def get_queryset(self):
         #print('delete get_queryset called')
         qs = super(OwnerDeleteView, self).get_queryset()
-        # return qs.filter(owner=self.request.user)
         if self.request.user.is_staff:
             return qs
         else:
