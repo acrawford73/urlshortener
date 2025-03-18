9808ae1e57cf09854bef706658f046f83b4c9daf
Anthony Crawford <crawfishmedia@gmail.com>
2025-03-18 01:18:01 -0300

    .

--- scripts/backupdb.sh
+++ scripts/backupdb.sh
@@ -6,3 +6,6 @@ TIMESTAMP=$(date +"%Y%m%d%H%M")
 
 # Dump the database with pg_dump and compress the output
 pg_dump -U $DB_USER $DB_NAME | gzip > "$BACKUP_DIR/${DB_NAME}_${TIMESTAMP}.sql.gz"
+
+# Delete backup files older than 15 days
+find $BACKUP_DIR -type f -name "${DB_NAME}_*.sql.gz" -mtime +15 -exec rm {} \;
