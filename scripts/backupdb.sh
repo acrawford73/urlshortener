abaab63c01cbbe8ee22a8b62a929d711ae3b7737
Anthony Crawford <crawfishmedia@gmail.com>
2025-03-23 16:49:50 -0300

    .

--- scripts/backupdb.sh
+++ scripts/backupdb.sh
@@ -5,7 +5,7 @@ BACKUP_DIR="/home/django/backups"
 TIMESTAMP=$(date +"%Y%m%d%H%M")
 
 # Dump the database with pg_dump and compress the output
-pg_dump -U $DB_USER $DB_NAME | gzip > "$BACKUP_DIR/${DB_NAME}_${TIMESTAMP}.sql.gz"
+pg_dump -U $DB_USER $DB_NAME -F c -b | gzip > "$BACKUP_DIR/${DB_NAME}_${TIMESTAMP}.sql.gz"
 
 # Delete backup files older than 15 days
 #find $BACKUP_DIR -type f -name "${DB_NAME}_*.sql.gz" -mtime +15 -exec rm {} \;
